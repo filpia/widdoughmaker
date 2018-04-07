327414e08ce40eee48ef9846cc611868790b2fbb
filpia <fpiasevoli@gmail.com>
2018-04-07 17:06:28 -0400

    converted to requests.session for login and requests

--- wid_tools.py
+++ wid_tools.py
@@ -23,55 +23,69 @@ class WIDTrader(object):
     API_STATIC_MARKET = "http://www.whiskyinvestdirect.com/view_market_xml.do"
     API_GET_BALANCE = "https://www.whiskyinvestdirect.com/secure/api/v2/view_balance_xml.do"
     API_GET_CHART_TEMPLATE = "https://www.whiskyinvestdirect.com/{distillery}/{bondYear}/{bondQuarter}/{barrelTypeCode}/chart.do"
+    
+    session = requests.Session()
+    raw_market_data = None
 
     def authenticate(self):
         config = json.load(open("config/configuration.txt"))
         user = config['username']
         pasw = config['password']
-        response = requests.get(self.API_GET_SESSION)
-        response = requests.post(self.API_POST_AUTHENTICATE, cookies=response.cookies, data={'j_username': user, 'j_password': pasw})
-        return response
+        creds = {'j_username': user, 'j_password': pasw}
+        
+        login_resp = self.session.get(self.API_GET_SESSION) 
+        login_auth_resp = self.session.post(self.API_POST_AUTHENTICATE, data = creds, headers = login_resp.cookies)
+
+        return login_auth_resp
 
     def get_raw_market_data(self):
-        http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED',ca_certs=certifi.where())
-        request = http.request("GET", self.API_GET_VIEW_MARKET)
-        xml_file = request.data
-        return xml_file
+        request = self.session.get(self.API_GET_VIEW_MARKET)
+        #xml_file = request.data
+        return request.content
     
     def get_static_market_data(self):
-        http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED',ca_certs=certifi.where())
-        request = http.request("GET", self.API_STATIC_MARKET)
-        xml_file = request.data
-        return xml_file
+        request = self.session.get(self.API_STATIC_MARKET)
+        
+        return request.content    
     
     def get_balance(self):
-        http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED',ca_certs=certifi.where())
-        request = http.request("GET", self.API_GET_BALANCE)
-        xml_file = request.data
-        return xml_file
+        request = self.session.get(self.API_GET_BALANCE)
+        
+        return request.content 
     
     def get_chart(self,attribs):
-        # {distillery}/{bondYear}/{bondQuarter}/{barrelTypeCode}
-        http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED',ca_certs=certifi.where())
-
-        request = http.request("GET", self.API_GET_CHART_TEMPLATE.format(**attribs))
-        xml_file = request.data
-        return xml_file
+        request = self.session.get(self.API_GET_CHART_TEMPLATE.format(**attribs))
+        
+        return request.content 
         
 
-    def get_market_data(self, security_class, security_id, currency):
-        root = ElementTree.fromstring(self.get_raw_market_data())
-
-        i = 0
-        for market in root.iter("pitch"):
-            sec_class = market.get("securityClassNarrative")
+    def get_market_data(self, security_id, currency):
+        if self.raw_market_data == None:
+            self.raw_market_data = ElementTree.fromstring(self.get_raw_market_data())
+            
+        for market in self.raw_market_data.iter("pitch"):
             sec_id = market.get("securityId")
             curr = market.get("considerationCurrency")
-            if sec_class == security_class and sec_id == security_id and curr == currency:
+            if sec_id == security_id and curr == currency:
+                dist = market.get('distillery')
+                cat = market.get('categoryName')
+                barrel = market.get('barrelTypeCode')
+                yr = market.get('bondYear')
+                qtr = market.get('bondQuarter')
+                
                 buy_price = list(list(market)[0])[0]
                 sell_price = list(list(market)[1])[0]
-                print("Buy: {0} at {1}".format(buy_price.get("quantity"), buy_price.get("limit")))
-                print("Sell: {0} at {1}".format(sell_price.get("quantity"), sell_price.get("limit")))
+                
+                return {'security_id':security_id,
+                        'distillery':dist,
+                        'category':cat,
+                        'barrel_type':barrel,
+                        'year':yr,
+                        'qtr':qtr,
+                        'current_buy_price':float(buy_price.get("limit")),
+                        'current_sell_price':float(sell_price.get("limit")),
+                        'currency':currency,
+                        'notes':'success'}
 
 
 class WIDDoughMaker():
@@ -128,15 +142,21 @@ class WIDDoughMaker():
         attribs_df = pd.DataFrame([attribs])
         attribs_df['dummy'] = 1
         
-        return chart_df.merge(attribs_df,on='dummy')
+        return chart_df.merge(attribs_df,ons='dummy')
     
     def pitches(self):
-        return self.all_pitches_in_mkt
+        return self.all_pitches_in_mkt()
     
+    def market_rate(self,secID='SPIRIT000218',curr='USD'):
+        try:
+            return self.trader.get_market_data(secID,curr)
+        except:
+            return {'security_id':secID,
+                        'notes':'failure'}
         
     def login(self):
         response = self.trader.authenticate()
 
         self.logged_session = response
         
-        return
+        return response
