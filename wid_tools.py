629fe9d5c2d9c4c5072951e5f4e05df1d26e58ee
Filip Piasevoli <filippiasevoli@transfix.io>
2019-04-07 15:47:05 -0400

    add function to trigger lambda event

--- wid_tools.py
+++ wid_tools.py
@@ -5,11 +5,12 @@ import datetime
 import time
 from xml.etree import ElementTree
 import requests
-import urllib3
 import configparser
 import json
 import pandas as pd
+import boto3
 import ast
+import os
 
 class WIDTrader(object):
 
@@ -19,17 +20,25 @@ class WIDTrader(object):
     API_STATIC_MARKET = "http://www.whiskyinvestdirect.com/view_market_xml.do"
     API_GET_BALANCE = "https://www.whiskyinvestdirect.com/secure/api/v2/view_balance_xml.do"
     API_GET_CHART_TEMPLATE = "https://www.whiskyinvestdirect.com/{distillery}/{bondYear}/{bondQuarter}/{barrelTypeCode}/chart.do"
-    
+    S3_BUCKET = "wid-prices"
+
     session = requests.Session()
     raw_market_data = None
+    s3_session = boto3.Session()
+    s3 = s3_session.client('s3')
 
     def authenticate(self):
-        config = json.load(open("config/configuration.txt"))
-        user = config['username']
-        pasw = config['password']
-        creds = {'j_username': user, 'j_password': pasw}
-        
-        login_resp = self.session.get(self.API_GET_SESSION) 
+
+        if not (os.environ.get('WID_USERNAME') and os.environ.get('WID_PW')):
+            config = json.load(open("config/configuration.txt"))
+            user = config['username']
+            pw = config['password']
+        else:
+            user = os.environ.get('WID_USERNAME')
+            pw = os.environ.get('WID_PW')
+        creds = {'j_username': user, 'j_password': pw}
+
+        login_resp = self.session.get(self.API_GET_SESSION)
         login_auth_resp = self.session.post(self.API_POST_AUTHENTICATE, data = creds, headers = login_resp.cookies)
 
         return login_auth_resp
@@ -38,110 +47,122 @@ class WIDTrader(object):
         request = self.session.get(self.API_GET_VIEW_MARKET)
         #xml_file = request.data
         return request.content
-    
+
     def get_static_market_data(self):
         request = self.session.get(self.API_STATIC_MARKET)
-        
-        return request.content    
-    
+
+        return request.content
+
     def get_balance(self):
         request = self.session.get(self.API_GET_BALANCE)
-        
-        return request.content 
-    
+
+        return request.content
+
+
     def get_chart(self,attribs):
         request = self.session.get(self.API_GET_CHART_TEMPLATE.format(**attribs))
-        
-        return request.content 
-        
 
-    def get_market_data(self, security_id, currency):
+        return request.content
+
+
+    def put_to_s3(self, bucket, key, fpath):
+        f_obj = self.s3.put_object(Bucket=bucket, Key=key, Body=fpath)
+        return
+
+
+    def log_market_data(self):
+        now = datetime.datetime.now()
+        md = self.get_market_data()
+        self.put_to_s3(self.S3_BUCKET, now.strftime('%Y/%m/%d/prices_%H%M%S.csv'), md.to_csv())
+        return
+
+
+    def get_market_data(self):
         if self.raw_market_data == None:
             self.raw_market_data = ElementTree.fromstring(self.get_raw_market_data())
-            
-        for market in self.raw_market_data.iter("pitch"):
-            sec_id = market.get("securityId")
-            curr = market.get("considerationCurrency")
-            if sec_id == security_id and curr == currency:
-                dist = market.get('distillery')
-                cat = market.get('categoryName')
-                barrel = market.get('barrelTypeCode')
-                yr = market.get('bondYear')
-                qtr = market.get('bondQuarter')
-                
-                buy_price = list(list(market)[0])[0]
-                sell_price = list(list(market)[1])[0]
-                
-                return {'security_id':security_id,
-                        'distillery':dist,
-                        'category':cat,
-                        'barrel_type':barrel,
-                        'year':yr,
-                        'qtr':qtr,
-                        'current_buy_price':float(buy_price.get("limit")),
-                        'current_sell_price':float(sell_price.get("limit")),
-                        'currency':currency,
-                        'notes':'success'}
+        records = []
+        for pitch in self.raw_market_data.iter("pitch"):
+            rec = {'security_id': pitch.get("securityId"),
+                    'distillery': pitch.get('distillery'),
+                    'category': pitch.get('categoryName'),
+                    'barrel_type': pitch.get('barrelTypeCode'),
+                    'year': pitch.get('bondYear'),
+                    'qtr': pitch.get('bondQuarter'),
+                    'currency': pitch.get("considerationCurrency")}
+            purchase_options = list(list(pitch)[0])
+            sell_options = list(list(pitch)[1])
+            i = 0
+            for b in purchase_options:
+                rec[f'po_{i}_qty'] = b.get('quantity')
+                rec[f'po_{i}_price'] = b.get('limit')
+                i+=1
+            i = 0
+            for s in sell_options:
+                rec[f'so_{i}_qty'] = s.get('quantity')
+                rec[f'so_{i}_price'] = s.get('limit')
+                i+=1
+            records.append(rec)
+        return pd.DataFrame(records)
 
 class WIDDoughMaker():
-    
+
     def setUp(self):
         self.trader = WIDTrader()
         self.logged_session = None
         self.pitch_path = "pitches.xml"
         self.all_pitches_in_mkt = None
-            
+
     def fetch_pitches(self):
         content = self.trader.get_static_market_data()
-        
+
         with open(self.pitch_path, "w") as f:
-            f.write(content)       
+            f.write(content)
         return
-    
+
     def get_pitches(self):
         self.fetch_pitches()
-        
+
         tree = ElementTree.parse(self.pitch_path)
         root = tree.getroot()
-        
+
         pitches = []
-        
+
         for pitch in root.iter('pitch'):
             pitches.append(pitch.attrib)
-        
+
         r = pd.DataFrame(pitches)
         r['distillery'] = r['distillery'].str.lower()
-        
+
         self.all_pitches_in_mkt = r.drop_duplicates(['barrelTypeCode','bondQuarter',
                                                      'bondYear','categoryName',
                                                      'distillery'])
         print("Num of pitches:{}".format(self.all_pitches_in_mkt.shape[0]))
-        
+
         return
-    
+
     def get_chart_data(self, attribs = None):
         #return self.all_pitches_in_mkt.iloc[0]
-        
+
         if attribs == None:
             attribs = self.all_pitches_in_mkt.iloc[0].to_dict()
-        
-        r = self.trader.get_chart(attribs) 
-            
+
+        r = self.trader.get_chart(attribs)
+
         # want to clip xml string
-        
+
         chart_data = r.split("Chart.drawChart( $('#chartContainer'), ")[1].split(", 'USD'")[0]
-        chart_df = pd.DataFrame(ast.literal_eval(chart_data))        
+        chart_df = pd.DataFrame(ast.literal_eval(chart_data))
         chart_df['dealDate'] = chart_df['dealDate'].apply(lambda x: datetime.datetime.fromtimestamp(x/1000).date() )
         chart_df['dummy'] = 1
-        
+
         attribs_df = pd.DataFrame([attribs])
         attribs_df['dummy'] = 1
-        
+
         return chart_df.merge(attribs_df,ons='dummy')
-    
+
     def pitches(self):
         return self.all_pitches_in_mkt()
-    
+
     def market_rate(self,secID='SPIRIT000218',curr='USD'):
         try:
             r = self.trader.get_market_data(secID,curr)
@@ -153,42 +174,41 @@ class WIDDoughMaker():
         except:
             return {'security_id':secID,
                         'notes':'failure'}
-        
+
     def login(self):
         response = self.trader.authenticate()
 
         self.logged_session = response
-        
+
         return response
-    
+
     def alerts_helper(self,secID,base_price,alert_thresh_pct):
         mr = self.market_rate(secID,'USD')
-        
+
         if mr['current_buy_price']>base_price*(1+alert_thresh_pct):
             return mr
         else:
             return {}
-        
-    
+
+
     def alerts(self,alert_config):
         alert_df = pd.read_csv(alert_config)
         alert_items = ['barrel_type','distillery','qtr','year','security_id','current_buy_price']
 
         alerts = []
-        
+
         for f in alert_df.iterrows():
             whisky = f[1]
             alert_output = self.alerts_helper(whisky['security_id'],whisky['base_price'],whisky['notification_floor_pct'])
-            
+
             if alert_output != {}:
                 new_alert = {}
                 new_alert['security_id'] = whisky['security_id']
                 new_alert['base_price'] = whisky['base_price']
-                
+
                 for a in alert_items:
                     new_alert[a] = alert_output[a]
-                
+
                 alerts.append(new_alert)
-                
+
         return alerts
-            
