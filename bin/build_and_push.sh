85c4846cd3402e3a11cb34c5f20068a789d95d8f
Filip Piasevoli <filippiasevoli@transfix.io>
2023-10-08 10:45:30 -0400

    add line to authenticate

--- bin/build_and_push.sh
+++ bin/build_and_push.sh
@@ -3,6 +3,9 @@ set -euo pipefail
 
 TAG=$1
 
+# log into AWS and store creds in docker
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 400419513456.dkr.ecr.us-east-1.amazonaws.com
+
 # build local image
 docker build . -t widdoughmaker:$TAG
 
