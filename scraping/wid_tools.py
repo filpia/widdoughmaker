cf9c5ba01c642cd8e0283faff7a9a6bae22f0e2c
Filip Piasevoli <filippiasevoli@transfix.io>
2024-03-10 16:04:57 -0400

    add functionality to write to both file types to different buckets

--- scraping/wid_tools.py
+++ scraping/wid_tools.py
@@ -65,17 +65,16 @@ class WIDTrader(object):
         return
 
 
-    def log_market_data(self, s3_bucket, format='csv'):
+    def log_market_data(self, s3_bucket_dict, output_format='csv'):
+        assert output_format in ('csv', 'parquet', 'both')
         now = datetime.datetime.now()
         md = self.get_market_data()
-        if format == 'csv':
-            # self.put_to_s3(self.S3_BUCKET, now.strftime('%Y/%m/%d/prices_%H%M%S.csv'), md.to_csv())
-            md.to_csv(f's3://{s3_bucket}/{now.strftime("%Y/%m/%d/prices_%H%M%S.csv")}')
-        elif format == 'parquet':
-            md.to_parquet(f's3://{s3_bucket}/{now.strftime("%Y/%m/%d/prices_%H%M%S.parquet")}')
-        else:
-            raise ValueError('format must be one of csv or parquet')
-        return
+        if output_format in ('csv', 'both'):
+            s3_bucket = s3_bucket_dict['csv']
+            md.to_csv(f's3://{s3_bucket}/{now.strftime("%Y/%m/%d/prices_%H%M%S.csv")}', index=False)
+        if output_format in ('parquet', 'both'):
+            s3_bucket = s3_bucket_dict['parquet']
+            md.to_parquet(f's3://{s3_bucket}/{now.strftime("%Y/%m/%d/prices_%H%M%S.parquet")}', index=False)
 
 
     def get_market_data(self):
