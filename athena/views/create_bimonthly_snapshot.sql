70fb7a32edce79793c3edd8ed331e0c6142183a6
Filip Piasevoli <filippiasevoli@transfix.io>
2024-03-17 14:48:03 -0400

    update query for bimonthly snapshiot

--- athena/views/create_bimonthly_snapshot.sql
+++ athena/views/create_bimonthly_snapshot.sql
@@ -6,8 +6,8 @@ WITH
    , date_parse(replace(split(happened_at, ' ')[1], '/', '-'), '%Y-%m-%d') happened_at_date
    , date_parse(concat(concat(concat(CAST(year AS varchar), '-'), (CASE WHEN (qtr = 'Q1') THEN '01' WHEN (qtr = 'Q2') THEN '04' WHEN (qtr = 'Q3') THEN '07' WHEN (qtr = 'Q4') THEN '10' END)), '-01'), '%Y-%m-%d') spirit_date
    FROM
-     "AwsDataCatalog"."whiskey_invest_direct"."wid_prices_processed"
-   WHERE (split(split(happened_at, ' ')[1], '/')[3] IN ('01', '15')) AND (currency = 'GBP')
+     "AwsDataCatalog"."whiskey_invest_direct"."wid_prices_processed_parquet"
+   WHERE ((split(split(happened_at, ' ')[1], '/')[3] IN ('01', '15')) AND (currency = 'GBP'))
 ) 
 SELECT br.*
 FROM
@@ -20,4 +20,3 @@ INNER JOIN (
      bimonthly_records
    GROUP BY happened_at_date
 )  time_filt ON (br.happened_at = time_filt.happened_at))
-;
